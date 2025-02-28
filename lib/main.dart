19f0d1fc28a28fac32d72d8a397938d3caa982d6
RAM <shriramgupta8141@gmail.com>
2025-02-28 18:22:29 +0530

    made updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'package:alphabet/firebase_options.dart';
-import 'package:alphabet/functions/firebase_notifications_api.dart';
 import 'package:alphabet/screens/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await MobileAds.instance.initialize();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  setupPushNotifications();
+void main() {
   runApp(const MyApp());
 }
 
